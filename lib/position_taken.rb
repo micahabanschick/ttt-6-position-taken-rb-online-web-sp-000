dc77038b67224407e613deb87e5b2e3ab2619c19
Micah Banschick <micha.ban@gmail.com>
2020-02-13 21:17:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,12 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  false if board[index] == " "
+  if board[index] == " "
+    false 
+  elsif board[index] == ""
+    false 
+  elsif board[index] == nil 
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end 
 end
